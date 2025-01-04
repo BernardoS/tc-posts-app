47687be6c8fc2bf57d540ec013be96137c9804e5
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-01-04 18:10:06 -0300

    Criação do projeto e rotas iniciais

---
+++ src/app/_layout.tsx
@@ -0,0 +1,33 @@
+import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
+import { FontAwesome } from "@expo/vector-icons";
+import { Drawer } from "expo-router/drawer";
+
+export default function Layout() {
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer>
+                <Drawer.Screen
+                    name="index"
+                    options={{
+                        headerShown: false,
+                        drawerIcon: ({size,color}) => <FontAwesome size={size} color={color} name="home" />,
+                        drawerLabel:'Home'
+                    }} />
+                <Drawer.Screen
+                    name="login"
+                    options={{
+                        headerShown: false,
+                        drawerIcon: ({size,color}) => <FontAwesome size={size} color={color} name="user" />,
+                        drawerLabel:'Área do professor'
+                    }} />
+                <Drawer.Screen
+                    name="search"
+                    options={{
+                        headerShown: false,
+                        drawerIcon: ({size,color}) => <FontAwesome size={size} color={color}  name="search" />,
+                        drawerLabel:'Pesquisar posts'
+                    }} />
+            </Drawer>
+        </GestureHandlerRootView>
+    )
+}

6d7b6f7a0b19cc0097a5c0ba33a1053934f66dd6
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-01-16 22:49:03 -0300

    Banner principal e lista de posts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,31 +2,51 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { FontAwesome } from "@expo/vector-icons";
 import { Drawer } from "expo-router/drawer";
 import 'react-native-gesture-handler';
+import { useEffect, useState } from "react";
+import * as Font from 'expo-font';
 
 export default function Layout() {
+
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            await Font.loadAsync({
+                'MavenPro-Bold': require('../assets/fonts/MavenPro-Bold.ttf'),
+                'MavenPro-ExtraBold': require('../assets/fonts/MavenPro-ExtraBold.ttf'),
+                'MavenPro-SemiBold': require('../assets/fonts/MavenPro-SemiBold.ttf'),
+            });
+            setFontsLoaded(true);
+        })();
+    }, []);
+
+    if (!fontsLoaded) {
+        return null; // Ou exibir um carregamento
+    }
+
     return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{ flex: 1,}}>
             <Drawer>
                 <Drawer.Screen
                     name="index"
                     options={{
                         headerShown: false,
-                        drawerIcon: ({size,color}) => <FontAwesome size={size} color={color} name="home" />,
-                        drawerLabel:'Home'
+                        drawerIcon: ({ size, color }) => <FontAwesome size={size} color={color} name="home" />,
+                        drawerLabel: 'Home'
                     }} />
                 <Drawer.Screen
                     name="login"
                     options={{
                         headerShown: false,
-                        drawerIcon: ({size,color}) => <FontAwesome size={size} color={color} name="user" />,
-                        drawerLabel:'Área do professor'
+                        drawerIcon: ({ size, color }) => <FontAwesome size={size} color={color} name="user" />,
+                        drawerLabel: 'Área do professor'
                     }} />
                 <Drawer.Screen
                     name="search"
                     options={{
                         headerShown: false,
-                        drawerIcon: ({size,color}) => <FontAwesome size={size} color={color}  name="search" />,
-                        drawerLabel:'Pesquisar posts'
+                        drawerIcon: ({ size, color }) => <FontAwesome size={size} color={color} name="search" />,
+                        drawerLabel: 'Pesquisar posts'
                     }} />
             </Drawer>
         </GestureHandlerRootView>

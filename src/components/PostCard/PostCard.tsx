218e5af61057880b80392d74f7c8b06b5e1d431d
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-01-31 20:26:38 -0300

    Ajuste no card do post na home

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -2,19 +2,24 @@ import {
     PostCardContainer,
     PostCardImage,
     PostCardContent,
-    PostCardTitle
+    PostCardTitle,
+    PostCardLine,
+    PostCardSubtitile,
+    PostDescriptionContainer
 } from "./PostCardStyle";
 import LogoIcon from "../../assets/images/logo-icon.png"
 import { router } from "expo-router";
+import React from "react";
 
 interface PostCardProps {
-    id: string,
+    id?: string,
     coverImage?: string,
-    title: string
+    title?: string,
+    description?: string
 }
 
 
-const PostCard: React.FC<PostCardProps> = ({ id, coverImage, title }) => {
+const PostCard: React.FC<PostCardProps> = ({ id, coverImage, title, description }) => {
 
     let cardImage = '';
 
@@ -22,11 +27,35 @@ const PostCard: React.FC<PostCardProps> = ({ id, coverImage, title }) => {
         cardImage = coverImage
     }
 
+    function reduzirTexto(texto?:string, limite?:number) {
+        
+        if(texto && limite){
+            if (texto.length <= limite) return texto;
+      
+            let reduzido = texto.slice(0, limite);
+            
+            // Se o caractere seguinte não for um espaço, corta até o último espaço antes do limite
+            if (texto[limite] !== ' ') {
+              reduzido = reduzido.slice(0, reduzido.lastIndexOf(' '));
+            }
+            return reduzido + "...";
+        }
+        return "..."
+      }
+
     return (
-        <PostCardContainer id={id}onPress={()=>router.navigate(`/private/post/${id}`)} >
+        <PostCardContainer id={id} onPress={() => router.navigate(`/private/post/${id}`)} >
             <PostCardImage source={LogoIcon} alt={title} className="post-card-image" />
             <PostCardContent>
-                <PostCardTitle className="post-card-title">{title}</PostCardTitle>
+                <PostCardTitle className="post-card-title">{reduzirTexto(title,80)}</PostCardTitle>
+                <PostCardLine></PostCardLine>
+                {description && (
+                    <PostDescriptionContainer>
+                        <PostCardSubtitile>
+                            {reduzirTexto(description,120)}
+                        </PostCardSubtitile>
+                    </PostDescriptionContainer>
+                )}
             </PostCardContent>
         </PostCardContainer>
     );

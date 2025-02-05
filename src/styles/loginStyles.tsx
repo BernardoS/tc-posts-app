196739aff8b43d9a7b130c29418819e2e2104d6d
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-02-05 19:57:07 -0300

    Ajuste de estilização da página de login

--- src/styles/loginStyles.tsx
+++ src/styles/loginStyles.tsx
@@ -71,7 +71,7 @@ export const RegisterButton = styled.TouchableOpacity`
     align-items:center;
     justify-content:center;
     gap:8px;
-    height:48px;
+    height:64px;
     font-weight:700;
     border-radius:10px;
     padding:8px 16px;
@@ -80,13 +80,13 @@ export const RegisterButton = styled.TouchableOpacity`
 `;
 
 export const LoginButton = styled.TouchableOpacity`
-    background-color:#FCC918;
+    background-color: #FCC918;
     border:2px solid #08244B;
     display:flex;
     align-items:center;
     justify-content:center;
     gap:8px;
-    height:48px;
+    height:64px;
     font-weight:700;
     border-radius:10px;
     padding:8px 16px;
